75663392bda44beb6b7a23b16682f55834a3a4b6
Srinivas <lingampellisrinivas3036@gmail.com>
2025-06-24 22:19:09 +0530

    Streams with the Terminal Operations is Added

---
+++ lambda/Streams/Streams2.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class Streams2 {
+
+
+    public static void main(String[] args) {
+        System.out.println("================Terminal Operations==================");
+
+               List<String> names = Arrays.asList(
+            "Reflection", "Collection", "Stream",
+            "Structure", "Sorting", "State"
+        );
+
+        System.out.println("==========For Each ============");
+
+        names.stream().forEach(System.out::println);
+
+        System.out.println("===========Collect============");
+
+        List<String> result=names.stream()
+        .filter(name->name.startsWith("S"))
+        .collect(Collectors.toList());
+
+            result.stream().forEach(System.out::println);
+
+
+            System.out.println("============== Reduce ============= ");
+            String concateNames=names.stream().reduce("", (partial,actual)->partial+" "+actual);
+            System.out.println(concateNames.trim());
+
+            System.out.println("=================Count=============");
+            long count=names.stream().count();
+            System.out.println(count);
+
+            System.out.println("============find first================");
+            Optional<String> s=names.stream().findFirst();
+            s.ifPresent(System.out::println);
+
+    }
+    
+}
