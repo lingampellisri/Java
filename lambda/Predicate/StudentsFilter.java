dcf0746eda4d8e3ae00353f0f5003282d7865179
Srinivas <lingampellisrinivas3036@gmail.com>
2025-06-25 08:14:09 +0530

    Students With Start is Sri and End is a is added

--- lambda/Predicate/StudentsFilter.java
+++ lambda/Predicate/StudentsFilter.java
@@ -4,13 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
+import javax.xml.stream.events.StartDocument;
+
 public class StudentsFilter {
     public static void main(String[] args) {
 
         List<String> Students = new ArrayList<>();
-        Students.add("Srinivas");
+        Students.add("Srinivasa");
         Students.add("Venu");
-        Students.add("Srinu");
+        Students.add("Srinu Kinga");
         Students.add("Sri");
         Students.add("Sri Krishna");
         Students.add("Venkatesh");
@@ -19,10 +21,28 @@ public class StudentsFilter {
         Students.add("Vinay");
 
         Predicate<String>startsWithSri=name->name.startsWith("Sri");
+        Predicate<String>endsWithA=name->name.endsWith("a");
+
+        // boolean result=startsWithSri.and(endsWithA).test(
+        //   "Sri Krishna"
+        // );
+
+        // System.out.println(result);
+System.out.println("================Student with Sri Filtered================");
 
         Students.stream()
         .filter(startsWithSri)
         .forEach(System.out::println);
+
+
+        System.out.println("===================Students With Start is Sri and End is a==================");
+
+        Students.stream()
+        .filter(startsWithSri.and(endsWithA))
+        .forEach(System.out::println);
+
+
+
 
 
     }
